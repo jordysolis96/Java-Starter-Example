b290375a2fa79bc40d8490f74c7bda939c693116
jordy muniz <jordy.muniz96@gmail.com>
2021-05-14 13:41:35 -0500

    "pushing on eclipse and calculator class"

---
+++ HelloWorldExample/src/com/qa/startworld/Calculator.java
@@ -0,0 +1,19 @@
+package com.qa.startworld;
+
+public class Calculator {
+	public static int add(int num, int num2) {
+		return num + num2;
+	}
+	
+	public static int Multiplication(int num, int num2) {
+		return num * num2;
+	}
+	
+	public static int Subtraction(int num, int num2) {
+		return num - num2;
+	}
+	
+	public static Double Divsion(Double num, Double num2) {
+		return num / num2;
+	}
+}
