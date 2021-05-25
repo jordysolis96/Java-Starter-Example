91f5444fb276743961a22bd5a8c7f6b65144a909
jordy muniz <jordy.muniz96@gmail.com>
2021-05-25 11:10:10 -0500

    update

--- git@github.com:jordysolis96/Junit.git/JunitExample/src/main/java/com/qa/Calculator.java
+++ git@github.com:jordysolis96/Junit.git/JunitExample/src/main/java/com/qa/Calculator.java
@@ -1,5 +1,24 @@
 package com.qa;
 
 public class Calculator {
+	
+	public Calculator() {
+		
+	}
+	public static int add(int a,int b) {
+		return a+b;
+	}
+	public static int subtract(int a,int b) {
+		return a-b;
+	}
+	public static int multiply(int a,int b) {
+		return a*b;
+	}
+	public static float divide(float a,float b) {
+		return a/b;
+	}
+	public static int modulus(int a,int b) {
+		return a%b;
+	}
 
 }
--- git@github.com:jordysolis96/Junit.git/JunitExample/src/test/java/JunitExampleTest/JunitExampleTest.java
+++ git@github.com:jordysolis96/Junit.git/JunitExample/src/test/java/JunitExampleTest/JunitExampleTest.java
@@ -1,5 +1,52 @@
 package JunitExampleTest;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.qa.Blackjack;
+import com.qa.Calculator;
+
 public class JunitExampleTest {
 
+	
+	@Test
+	public void addTest() {
+		assertEquals(4, Calculator.add(2, 2));
+	}
+	
+	@Test
+	public void subTest() {
+		assertEquals(10, Calculator.subtract(0, 10));
+		assertEquals(4, Calculator.subtract(4, 4));
+	}
+	
+	@Test
+	public void multTest() {
+		assertEquals(6, Calculator.multiply(2, 3));
+		assertEquals(-6, Calculator.multiply(2, -3));
+	}
+	
+	@Test
+	public void modTest() {
+		assertEquals(2, Calculator.modulus(10, 4));
+	}
+	
+	@Test
+	public void bjTest() {
+		assertEquals(20, Blackjack.play(20, 20));
+	}
+	@Test
+	public void bjTest2() {		
+		assertEquals(0, Blackjack.play(23, 22));
+	}
+	@Test
+	public void bj3() {
+		assertEquals(15, Blackjack.play(15, 22));
+	}
+	@Test
+	public void bj4() {
+		assertEquals(12, Blackjack.play(22, 12));
+	}
+	
 }
